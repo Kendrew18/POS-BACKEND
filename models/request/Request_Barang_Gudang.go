9a8c6329efd128d808730508d901491065a8261d
Kendrew18 <kendrewtandio71@gmail.com>
2023-12-20 14:50:09 +0700

    commit 19-12-23 pt3

--- models/request/Request_Barang_Gudang.go
+++ models/request/Request_Barang_Gudang.go
@@ -7,4 +7,6 @@ type Update_Status_Request_Barang_Request struct {
 type Update_Status_Kode_Request_Barang_Request struct {
 	Kode_request_barang_kasir string `json:"kode_request_barang_kasir"`
 	Kode_user                 string `json:"kode_user"`
+	Kode_gudang               string `json:"kode_gudang"`
+	Kode_nota                 string `json:"kode_nota"`
 }
--- services/gudang/request_barang_gudang/Request_Barang_Gudang.go
+++ services/gudang/request_barang_gudang/Request_Barang_Gudang.go
@@ -1,156 +1,160 @@
 package request_barang_gudang
 
-// func Update_Status_Refund(Request request_kasir.Update_Status_Request_Barang_Kasir, Request_kode request_kasir.Kode_Request_Barang_Kasir_Request) (response.Response, error) {
-// 	var res response.Response
-// 	//var err2 error
-// 	con := db.CreateConGorm().Table("request_barang_kasir")
-// 	status := -1
+import (
+	"POS-BACKEND/db"
+	"POS-BACKEND/models/request"
+	"POS-BACKEND/models/request_kasir"
+	"POS-BACKEND/models/response"
+	"POS-BACKEND/services/gudang/stock_keluar"
+	"fmt"
+	"net/http"
+	"time"
+)
 
-// 	err := con.Select("status").Where("kode_request_barang_kasir = ?", Request_kode.Kode_request_barang_kasir).Scan(&status)
+func Update_Status_Refund(Request request_kasir.Update_Status_Request_Barang_Kasir, Request_kode request.Update_Status_Kode_Request_Barang_Request) (response.Response, error) {
+	var res response.Response
+	//var err2 error
+	con := db.CreateConGorm().Table("request_barang_kasir")
+	status := -1
 
-// 	if err.Error != nil {
-// 		res.Status = http.StatusNotFound
-// 		res.Message = "Status Not Found"
-// 		res.Data = Request
-// 		return res, err.Error
-// 	}
+	err := con.Select("status").Where("kode_request_barang_kasir = ?", Request_kode.Kode_request_barang_kasir).Scan(&status)
 
-// 	if status != 3 {
-// 		if Request.Status == 1 || (Request.Status == 4 && status == 0) {
+	if err.Error != nil {
+		res.Status = http.StatusNotFound
+		res.Message = "Status Not Found"
+		res.Data = Request
+		return res, err.Error
+	}
 
-// 			con := db.CreateConGorm().Table("request_barang_kasir")
+	if status != 3 {
+		if Request.Status == 1 || (Request.Status == 4 && status == 0) {
 
-// 			err := con.Where("kode_request_barang_kasir = ?", Request_kode.Kode_request_barang_kasir).Select("status").Updates(&Request)
+			con := db.CreateConGorm().Table("request_barang_kasir")
 
-// 			if err.Error != nil {
-// 				res.Status = http.StatusNotFound
-// 				res.Message = "Status Not Found"
-// 				res.Data = Request
-// 				return res, err.Error
-// 			} else {
-// 				res.Status = http.StatusOK
-// 				res.Message = "Suksess"
-// 				res.Data = map[string]int64{
-// 					"rows": err.RowsAffected,
-// 				}
-// 			}
+			err := con.Where("kode_request_barang_kasir = ?", Request_kode.Kode_request_barang_kasir).Select("status").Updates(&Request)
 
-// 		} else if Request.Status == 2 {
-// 			var data_update request.Data_Update_Refund
-// 			con := db.CreateConGorm().Table("refund")
+			if err.Error != nil {
+				res.Status = http.StatusNotFound
+				res.Message = "Status Not Found"
+				res.Data = Request
+				return res, err.Error
+			} else {
+				res.Status = http.StatusOK
+				res.Message = "Suksess"
+				res.Data = map[string]int64{
+					"rows": err.RowsAffected,
+				}
+			}
 
-// 			date := time.Now()
-// 			tanggal_sekarang := date.Format("2006-01-02")
+		} else if Request.Status == 2 {
+			con := db.CreateConGorm().Table("request_barang_kasir")
 
-// 			fmt.Println(tanggal_sekarang)
-// 			data_update.Status = Request.Status
-// 			data_update.Tanggal_pengembalian = tanggal_sekarang
+			date := time.Now()
+			tanggal_sekarang := date.Format("2006-01-02")
 
-// 			err := con.Where("kode_request_barang_kasir = ?", Request_kode.Kode_request_barang_kasir).Select("status").Updates(&Request)
+			err := con.Where("kode_request_barang_kasir = ?", Request_kode.Kode_request_barang_kasir).Select("status").Updates(&Request)
 
-// 			if err.Error != nil {
-// 				res.Status = http.StatusNotFound
-// 				res.Message = "Status Not Found"
-// 				res.Data = Request
-// 				return res, err.Error
-// 			}
+			if err.Error != nil {
+				res.Status = http.StatusNotFound
+				res.Message = "Status Not Found"
+				res.Data = Request
+				return res, err.Error
+			}
 
-// 			var Request_Input_stock request.Input_Stock_Keluar_Request
+			var Request_Input_stock request.Input_Stock_Keluar_Request
 
-// 			var Temp request.Tanggal_dan_Kode_Gudang
+			con_user := db.CreateConGorm().Table("user")
 
-// 			con_user := db.CreateConGorm().Table("user")
+			penanggung_jawab := ""
 
-// 			penanggung_jawab := ""
+			err = con_user.Select("username").Where("id_user = ?", Request_kode.Kode_user).Scan(&penanggung_jawab)
 
-// 			err = con_user.Select("username").Where("id_user = ?", Request_kode.Kode_user).Scan(&penanggung_jawab)
+			Request_Input_stock.Kode_gudang = Request_kode.Kode_gudang
+			Request_Input_stock.Tanggal = tanggal_sekarang
+			Request_Input_stock.Nama_penanggung_jawab = penanggung_jawab
+			Request_Input_stock.Kode_nota = Request_kode.Kode_nota
 
-// 			err = con.Select("tanggal_pengembalian", "kode_gudang").Where("kode_refund = ?", Request_kode.Kode_refund).Scan(&Temp)
+			if err.Error != nil {
+				res.Status = http.StatusNotFound
+				res.Message = "Status Not Found"
+				res.Data = Request
+				return res, err.Error
+			}
 
-// 			Request_Input_stock.Kode_gudang = Temp.Kode_gudang
-// 			Request_Input_stock.Tanggal = Temp.Tanggal_pengembalian
-// 			Request_Input_stock.Nama_penanggung_jawab = penanggung_jawab
+			err = con.Select("kode_kasir").Where("kode_request_barang_kasir = ?", Request_kode.Kode_request_barang_kasir).Scan(&Request_Input_stock.Kode)
 
-// 			if err.Error != nil {
-// 				res.Status = http.StatusNotFound
-// 				res.Message = "Status Not Found"
-// 				res.Data = Request
-// 				return res, err.Error
-// 			}
+			if err.Error != nil {
+				res.Status = http.StatusNotFound
+				res.Message = "Status Not Found"
+				res.Data = Request
+				return res, err.Error
+			}
 
-// 			err = con.Select("kode_supplier").Where("kode_refund = ?", Request_kode.Kode_refund).Scan(&Request_Input_stock.Kode)
+			fmt.Println(Request_Input_stock.Kode)
 
-// 			if err.Error != nil {
-// 				res.Status = http.StatusNotFound
-// 				res.Message = "Status Not Found"
-// 				res.Data = Request
-// 				return res, err.Error
-// 			}
+			con_barang := db.CreateConGorm().Table("barang_request_barang_kasir")
+			rows, err2 := con_barang.Select("kode_stock_gudang", "jumlah").Where("kode_refund = ?", Request_kode.Kode_request_barang_kasir).Rows()
 
-// 			fmt.Println(Request_Input_stock.Kode)
+			defer rows.Close()
 
-// 			con_barang := db.CreateConGorm().Table("barang_refund")
-// 			rows, err2 := con_barang.Select("kode_nota", "kode_stock", "jumlah").Where("kode_refund = ?", Request_kode.Kode_refund).Rows()
+			if err2 != nil {
+				res.Status = http.StatusNotFound
+				res.Message = "Status Not Found"
+				res.Data = Request
+				return res, err2
+			}
 
-// 			defer rows.Close()
+			kode_stock := ""
+			jumlah_barang := ""
+			harga := ""
 
-// 			if err2 != nil {
-// 				res.Status = http.StatusNotFound
-// 				res.Message = "Status Not Found"
-// 				res.Data = Request
-// 				return res, err2
-// 			}
+			for rows.Next() {
+				var data request.Move_Refund_To_Stock_Keluar_Request
+				err2 = rows.Scan(&data.Kode_stock, &data.Jumlah_barang)
+				fmt.Println(Request_Input_stock)
 
-// 			for rows.Next() {
-// 				kode_stock := ""
-// 				jumlah_barang := ""
-// 				harga := ""
-// 				var data request.Move_Refund_To_Stock_Keluar_Request
-// 				err2 = rows.Scan(&Request_Input_stock.Kode_nota, &data.Kode_stock, &data.Jumlah_barang)
-// 				fmt.Println(Request_Input_stock)
+				if err2 != nil {
+					res.Status = http.StatusNotFound
+					res.Message = "Status Not Found"
+					res.Data = data
+					return res, err2
+				}
+				kode_stock = kode_stock + "|" + data.Kode_stock + "|"
 
-// 				if err2 != nil {
-// 					res.Status = http.StatusNotFound
-// 					res.Message = "Status Not Found"
-// 					res.Data = data
-// 					return res, err2
-// 				}
-// 				kode_stock = kode_stock + "|" + data.Kode_stock + "|"
+				jumlah_barang = jumlah_barang + "|" + fmt.Sprintf("%f", data.Jumlah_barang) + "|"
 
-// 				jumlah_barang = jumlah_barang + "|" + fmt.Sprintf("%f", data.Jumlah_barang) + "|"
+				harga = harga + "|" + "0" + "|"
+			}
 
-// 				harga = harga + "|" + "0" + "|"
+			var Request_Barang_V2 request.Input_Barang_Stock_Keluar_Request
 
-// 				var Request_Barang_V2 request.Input_Barang_Stock_Keluar_Request
+			Request_Barang_V2.Kode_stock = kode_stock
+			Request_Barang_V2.Jumlah_barang = jumlah_barang
+			Request_Barang_V2.Harga_jual = harga
 
-// 				Request_Barang_V2.Kode_stock = kode_stock
-// 				Request_Barang_V2.Jumlah_barang = jumlah_barang
-// 				Request_Barang_V2.Harga_jual = harga
+			fmt.Println(Request_Barang_V2)
 
-// 				fmt.Println(Request_Barang_V2)
+			res, err2 = stock_keluar.Input_Stock_Keluar(Request_Input_stock, Request_Barang_V2)
 
-// 				res, err2 = stock_keluar.Input_Stock_Keluar(Request_Input_stock, Request_Barang_V2)
-// 			}
+			if err2 != nil {
+				res.Status = http.StatusNotFound
+				res.Message = "Status Not Found"
+				res.Data = Request
+				return res, err.Error
+			} else {
+				res.Status = http.StatusOK
+				res.Message = "Suksess"
+				res.Data = map[string]int64{
+					"rows": err.RowsAffected,
+				}
+			}
 
-// 			if err2 != nil {
-// 				res.Status = http.StatusNotFound
-// 				res.Message = "Status Not Found"
-// 				res.Data = Request
-// 				return res, err.Error
-// 			} else {
-// 				res.Status = http.StatusOK
-// 				res.Message = "Suksess"
-// 				res.Data = map[string]int64{
-// 					"rows": err.RowsAffected,
-// 				}
-// 			}
+		}
+	} else {
+		res.Status = http.StatusNotFound
+		res.Message = "Tidah dapat di edit diakrenakan sudah sukses"
+		res.Data = Request
+	}
 
-// 		}
-// 	} else {
-// 		res.Status = http.StatusNotFound
-// 		res.Message = "Tidah dapat di edit diakrenakan sudah sukses"
-// 		res.Data = Request
-// 	}
-
-// 	return res, nil
-// }
+	return res, nil
+}
