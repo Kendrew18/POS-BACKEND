8f823b379b39897f27b9582467f59e9acfcc46c2
Kendrew18 <kendrewtandio71@gmail.com>
2024-01-12 20:13:28 +0700

    commit 12-01-24 pt3

--- services/kasir/kasir/Kasir.go
+++ services/kasir/kasir/Kasir.go
@@ -14,7 +14,7 @@ func Read_Menu_Kasir(Request request_kasir.Read_Stock_Kasir_Request) (response_k
 
 	con := db.CreateConGorm().Table("barang_kasir")
 
-	err := con.Select("kode_barang_kasir", "nama_barang_kasir", "nama_satuan", "jumlah", "harga").Joins("JOIN satuan_kasir sk ON sk.kode_satuan = barang_kasir.kode_satuan").Where("barang_kasir.kode_kasir = ? AND kode_store = ? AND jumlah > 0", Request.Kode_kasir, Request.Kode_store).Order("barang_kasir.co ASC").Scan(&data).Error
+	err := con.Select("kode_barang_kasir", "nama_barang_kasir", "nama_satuan", "jumlah*100/100 as jumlah", "harga").Joins("JOIN satuan_kasir sk ON sk.kode_satuan = barang_kasir.kode_satuan").Where("barang_kasir.kode_kasir = ? AND kode_store = ? AND jumlah > 0", Request.Kode_kasir, Request.Kode_store).Order("barang_kasir.co ASC").Scan(&data).Error
 
 	if err != nil {
 		res.Status = http.StatusNotFound
